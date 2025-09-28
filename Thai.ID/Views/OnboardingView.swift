4c7ad9d568c094ba3b591328c5e1f14feb3608af
Ittipat Pattum <nampu@finema.co>
2025-09-28 13:53:39 +0700

    save

--- Thai.ID/Views/OnboardingView.swift
+++ Thai.ID/Views/OnboardingView.swift
@@ -49,6 +49,4 @@ struct OnboardingView: View {
     }
 }
 
-#Preview {
-    OnboardingView().environment(\.locale, Locale(identifier: "th"))
-}
+
--- Thai.ID/Views/TermsView.swift
+++ Thai.ID/Views/TermsView.swift
@@ -1,18 +1,11 @@
-//
-//  TermsView.swift
-//  Thai.ID
-//
-//  Created by Pu on 27/9/2568 BE.
-//
-
 import SwiftUI
 
 struct TermsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    TermsView()
+    TermsView().environment(\.locale, Locale(identifier: "th"))
 }
