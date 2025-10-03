cdf28194ef963c209a470d8e6b3704a4129a7b7a
Ittipat Pattum <nampu@finema.co>
2025-10-03 17:25:07 +0700

    save

--- Thai.ID/Views/MainView.swift
+++ Thai.ID/Views/MainView.swift
@@ -1,33 +1,26 @@
-//
-//  MainView.swift
-//  Thai.ID
-//
-//  Created by Pu on 1/10/2568 BE.
-//
-
 import SwiftUI
 
 struct MainView: View {
     var body: some View {
         TabView {
-              HomeView()
-                  .tabItem {
-                      Image(systemName: "house.fill")
-                      Text("Home")
-                  }
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
 
             HistoryView()
-                  .tabItem {
-                      Image(systemName: "magnifyingglass")
-                      Text("Search")
-                  }
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+                }
 
-              ProfileView()
-                  .tabItem {
-                      Image(systemName: "person.fill")
-                      Text("Profile")
-                  }
-          }
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
+        }
     }
 }
 
