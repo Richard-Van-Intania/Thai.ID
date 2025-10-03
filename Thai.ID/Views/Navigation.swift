32b4beba4fcc2ff269ecfa25ebcbdc7ecdc62aae
Ittipat Pattum <nampu@finema.co>
2025-10-03 21:31:40 +0700

    save

---
+++ Thai.ID/Views/Navigation.swift
@@ -0,0 +1,8 @@
+//
+//  Navigation.swift
+//  Thai.ID
+//
+//  Created by Pu on 3/10/2568 BE.
+//
+
+import Foundation
---
+++ Thai.ID/Views/ProfileDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileDetailsView.swift
+//  Thai.ID
+//
+//  Created by Pu on 3/10/2568 BE.
+//
+
+import SwiftUI
+
+struct ProfileDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileDetailsView()
+}
