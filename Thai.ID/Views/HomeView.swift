001c52c04803ca3ed6f072c23267efc5ffe1d625
Ittipat Pattum <nampu@finema.co>
2025-10-04 21:00:24 +0700

    save

--- Thai.ID/Views/HomeView.swift
+++ Thai.ID/Views/HomeView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct HomeView: View {
     @Binding var path: NavigationPath
-    
+
     var body: some View {
         Button("debugs") {
             path.append(HomeRoute.profileDetailsView)
