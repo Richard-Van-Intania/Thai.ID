9daf80b1d4df99ac250f1beac824bcacec125577
Ittipat Pattum <nampu@finema.co>
2025-10-16 19:47:51 +0700

    save

--- Thai.ID/Views/Profile/PolicyAndSafetyView.swift
+++ Thai.ID/Views/Profile/PolicyAndSafetyView.swift
@@ -1,9 +1,29 @@
 import SwiftUI
+import WebKit
 
 struct PolicyAndSafetyView: View {
     @Binding var path: NavigationPath
 
     var body: some View {
-        Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
+        VStack {
+            WebView(url: URL(string: "https://thai.id/privacy")!).ignoresSafeArea()
+        }.frame(maxWidth: .infinity, maxHeight: .infinity).background(white).toolbar {
+            ToolbarItem(placement: .principal) {
+                Text("privacy_policy").font(.custom("FCIconicBold", size: 24)).foregroundColor(primary_black)
+            }
+        }.navigationBarTitleDisplayMode(.inline).toolbarBackground(white, for: .navigationBar).toolbarBackground(.visible, for: .navigationBar)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
     }
 }
