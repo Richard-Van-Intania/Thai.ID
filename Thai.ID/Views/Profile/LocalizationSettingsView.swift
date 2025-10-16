0de30c00b9f34ad4107471230f519190c07099f9
Ittipat Pattum <nampu@finema.co>
2025-10-16 21:13:38 +0700

    save

--- Thai.ID/Views/Profile/LocalizationSettingsView.swift
+++ Thai.ID/Views/Profile/LocalizationSettingsView.swift
@@ -6,7 +6,7 @@ struct LocalizationSettingsView: View {
 
     var body: some View {
         VStack {
-            Spacer().frame(height: 24)
+            Spacer().frame(height: 16)
             Group {
                 Button(action: {
                     if let url = URL(string: UIApplication.openSettingsURLString),
