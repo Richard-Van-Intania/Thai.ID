5a67e71fdb377250e632e2cfc36c63f09ea18083
Ittipat Pattum <nampu@finema.co>
2025-10-16 22:00:49 +0700

    save

--- Thai.ID/Views/Profile/SettingsView.swift
+++ Thai.ID/Views/Profile/SettingsView.swift
@@ -2,14 +2,29 @@ import SwiftUI
 
 struct SettingsView: View {
     @Binding var path: NavigationPath
+    @AppStorage("passcode") private var passcode: String = ""
+    @AppStorage("passcodeAsked") private var passcodeAsked: Bool = false
+    @State private var enabledPasscode = false
 
     var body: some View {
         VStack {
-            //
-        }.frame(maxWidth: .infinity, maxHeight: .infinity).background(white).toolbar {
+            HStack {
+                Text("enable_pin")
+                    .font(.custom("FCIconicBold", size: 20)).foregroundColor(primary_black)
+                Spacer()
+                Toggle(isOn: $enabledPasscode) {}.onChange(of: enabledPasscode) {
+                    print(enabledPasscode)
+                }
+            }.padding(.vertical)
+        }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top).padding(.horizontal).background(white).toolbar {
             ToolbarItem(placement: .principal) {
                 Text("login_settings").font(.custom("FCIconicBold", size: 24)).foregroundColor(primary_black)
             }
-        }.navigationBarTitleDisplayMode(.inline).toolbarBackground(white, for: .navigationBar).toolbarBackground(.visible, for: .navigationBar)
+        }.navigationBarTitleDisplayMode(.inline).toolbarBackground(white, for: .navigationBar).toolbarBackground(.visible, for: .navigationBar).onAppear {
+            enabledPasscode = !passcode.isEmpty && passcodeAsked
+        }
     }
 }
+
+
+// here
