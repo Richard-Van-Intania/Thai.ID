f1e69727f33e53a0396c00a62b82bec46f82875b
Ittipat Pattum <nampu@finema.co>
2025-10-18 21:42:45 +0700

    save

--- Thai.ID/Views/Passcode/EnterPasscodeChangeView.swift
+++ Thai.ID/Views/Passcode/EnterPasscodeChangeView.swift
@@ -1,3 +1,4 @@
+import BCryptSwift
 import SwiftUI
 
 struct EnterPasscodeChangeView: View {
@@ -12,6 +13,102 @@ struct EnterPasscodeChangeView: View {
     @State private var verifyPasscodeFailedDialog: Bool = false
 
     var body: some View {
-        Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("enter_current_pin").frame(maxWidth: .infinity).multilineTextAlignment(.center)
+                .font(.custom("FCIconicBold", size: 24)).foregroundColor(primary_black)
+            Spacer().frame(height: 48)
+            HStack(spacing: 24) {
+                ForEach(0...5, id: \.self) { index in
+                    Indicator(filled: index < passcodeModel.passcodeList.count)
+                }
+            }.modifier(Shake(animatableData: CGFloat(shakeCount)))
+            Spacer().frame(height: 48)
+            HStack(spacing: 24) {
+                CircleButton(label: 1, onButtonTap: { passcodeModel.passcodeAdd(code: 1) })
+                CircleButton(label: 2, onButtonTap: { passcodeModel.passcodeAdd(code: 2) })
+                CircleButton(label: 3, onButtonTap: { passcodeModel.passcodeAdd(code: 3) })
+            }
+            Spacer().frame(height: 24)
+            HStack(spacing: 24) {
+                CircleButton(label: 4, onButtonTap: { passcodeModel.passcodeAdd(code: 4) })
+                CircleButton(label: 5, onButtonTap: { passcodeModel.passcodeAdd(code: 5) })
+                CircleButton(label: 6, onButtonTap: { passcodeModel.passcodeAdd(code: 6) })
+            }
+            Spacer().frame(height: 24)
+            HStack(spacing: 24) {
+                CircleButton(label: 7, onButtonTap: { passcodeModel.passcodeAdd(code: 7) })
+                CircleButton(label: 8, onButtonTap: { passcodeModel.passcodeAdd(code: 8) })
+                CircleButton(label: 9, onButtonTap: { passcodeModel.passcodeAdd(code: 9) })
+            }
+            Spacer().frame(height: 24)
+            HStack(spacing: 24) {
+                Button(action: {
+                    passcodeModel.passcodeRestart()
+                }) {
+                    Image(systemName: "trash").font(.title2)
+                        .foregroundColor(primary_darkblue)
+                        .frame(width: 88, height: 88).contentShape(Circle())
+                }.buttonStyle(.plain)
+                CircleButton(label: 0, onButtonTap: { passcodeModel.passcodeAdd(code: 0) })
+                Button(action: {
+                    passcodeModel.passcodePop()
+                }) {
+                    Image(systemName: "delete.left").font(.title2)
+                        .foregroundColor(primary_darkblue)
+                        .frame(width: 88, height: 88).contentShape(Circle())
+                }.buttonStyle(.plain)
+            }
+            Spacer()
+            Button(action: {
+                path.removeLast()
+            }) {
+                Text("cancel")
+                    .font(.custom("FCIconicBold", size: 20)).foregroundColor(primary_darkblue)
+            }.buttonStyle(.plain)
+            Spacer().frame(height: 16)
+        }.navigationBarBackButtonHidden(true).alert(
+            "wrong",
+            isPresented: $verifyPasscodeFailedDialog,
+            actions: {
+                Button("ok", role: .none) {}
+            },
+        ).onAppear {
+            passcodeModel.allRestart()
+        }.onDisappear {
+            passcodeModel.allRestart()
+        }.onChange(of: passcodeModel.passcodeList) { oldValue, newValue in
+            if oldValue.count == 5 && newValue.count == 6 {
+                let concatenationPasscode = passcodeModel.passcodeConcatenation()
+                if !concatenationPasscode.isEmpty {
+                    do {
+                        let isValid = try BCryptSwiftModern.verifyPassword(concatenationPasscode, matchesHash: passcode)
+                        if isValid {
+                            authPassed()
+                        } else {
+                            authFailed()
+                        }
+                    } catch {
+                        authFailed()
+                        verifyPasscodeFailedDialog = false
+                    }
+                } else {
+                    authFailed()
+                }
+            }
+        }
+    }
+
+    func authFailed() {
+        isInvalid = true
+        withAnimation(.default) {
+            shakeCount += 1
+        }
+        passcodeModel.passcodeRestart()
+    }
+
+    func authPassed() {
+        isInvalid = false
+        //
     }
 }
