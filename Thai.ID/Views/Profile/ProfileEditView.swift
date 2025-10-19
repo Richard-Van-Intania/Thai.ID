ddaf837a59c05d554b31f47cb8ae1a25011a23e9
Ittipat Pattum <nampu@finema.co>
2025-10-19 22:07:21 +0700

    save

--- Thai.ID/Views/Profile/ProfileEditView.swift
+++ Thai.ID/Views/Profile/ProfileEditView.swift
@@ -1,16 +1,38 @@
 import SwiftUI
 
 struct ProfileEditView: View {
+    @AppStorage("idString") private var idString: String = ""
+    @AppStorage("thaiPrefix") private var thaiPrefix: String = ""
+    @AppStorage("thaiName") private var thaiName: String = ""
+    @AppStorage("thaiMiddleName") private var thaiMiddleName: String = ""
+    @AppStorage("thaiSurname") private var thaiSurname: String = ""
+    @AppStorage("engPrefix") private var engPrefix: String = ""
+    @AppStorage("engName") private var engName: String = ""
+    @AppStorage("engMiddleName") private var engMiddleName: String = ""
+    @AppStorage("engSurname") private var engSurname: String = ""
+    @AppStorage("birthDate") private var birthDate: String = ""
+
     @Binding var path: NavigationPath
 
     var body: some View {
-        Text( /*@START_MENU_TOKEN@*/"Hello, World!" /*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                HStack {
+                    //
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity).padding()
+            }
+            .background(white)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("id_card_info").font(.custom("FCIconicBold", size: 24)).foregroundColor(primary_black)
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline).toolbarBackground(white, for: .navigationBar).toolbarBackground(.visible, for: .navigationBar)
+        }
     }
 }
 
-
-
-
-//#Preview {
-//    ProfileEditView(path: .constant(NavigationPath())).environment(\.locale, Locale(identifier: "en"))
-//}
+#Preview {
+    ProfileEditView(path: .constant(NavigationPath())).environment(\.locale, Locale(identifier: "en"))
+}
