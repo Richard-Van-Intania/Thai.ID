e8a4e49d19508a048a5e2f110754d42711b4d44d
Ittipat Pattum <nampu@finema.co>
2025-10-23 13:34:45 +0700

    save

--- Thai.ID/Views/History/HistoryView.swift
+++ Thai.ID/Views/History/HistoryView.swift
@@ -9,11 +9,9 @@ struct HistoryView: View {
         ScrollView {
             VStack {
                 //
-            }.frame(maxWidth: .infinity, maxHeight: .infinity).background(neutral01)
-        }.toolbar {
-            ToolbarItem(placement: .principal) {
-                Text("history").font(.custom("FCIconicBold", size: 24)).foregroundColor(primary_black)
             }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity).background(neutral01).toolbar {
+            ToolbarItem(placement: .principal) { Text("history").font(.custom("FCIconicBold", size: 24)).foregroundColor(primary_black) }
         }.navigationBarTitleDisplayMode(.inline).toolbarBackground(white, for: .navigationBar).toolbarBackground(.visible, for: .navigationBar)
     }
 }
